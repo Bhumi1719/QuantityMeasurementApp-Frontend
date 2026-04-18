c1ff22834b64a45de32e424c3a9ab6c532e33595
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2026-04-18 11:43:41 +0530

    [Bhumika Yadav] .refactoring

--- src/app/services/history.service.ts
+++ src/app/services/history.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HistoryRecord } from '../models/qms.models';
-
-const API = 'http://localhost:8080/api/v1/quantities';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class HistoryService {
+  private apiUrl = `${environment.apiUrl}/api/v1/quantities`;
+
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<HistoryRecord[]> {
-    return this.http.get<HistoryRecord[]>(API);
+    return this.http.get<HistoryRecord[]>(this.apiUrl);
   }
 
   delete(id: number): Observable<string> {
-    return this.http.delete<string>(`${API}/${id}`);
+    return this.http.delete<string>(`${this.apiUrl}/${id}`);
   }
 }
